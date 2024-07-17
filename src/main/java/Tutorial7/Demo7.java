8a0cdbfed9e5f53bd207ccb3681afef483451fdf
prafpawar11 <prafulp1030@gmail.com>
2024-07-17 20:45:54 +0530

    Add user in Admin Page by Rahul

---
+++ src/main/java/Tutorial7/Demo7.java
@@ -0,0 +1,12 @@
+package Tutorial7;
+
+public class Demo7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Add user in Admin Page by Rahul");
+
+	}
+
+}
