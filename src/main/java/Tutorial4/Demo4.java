3dfde697ba1fc216c0416d323a6a29f06999b494
prafpawar11 <prafulp1030@gmail.com>
2024-07-17 20:29:20 +0530

    Add user TC by Dipali

---
+++ src/main/java/Tutorial4/Demo4.java
@@ -0,0 +1,12 @@
+package Tutorial4;
+
+public class Demo4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Add user Test cases by Dipali Automation tester");
+
+	}
+
+}
