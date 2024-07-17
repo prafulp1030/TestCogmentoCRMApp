0c80088e231845ed1516b50d9cf118a1e399c372
prafpawar11 <prafulp1030@gmail.com>
2024-07-17 20:42:33 +0530

    Delete user TC by Dipali

---
+++ src/main/java/Tutorial6/Demo6.java
@@ -0,0 +1,11 @@
+package Tutorial6;
+
+public class Demo6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Delete TC by Dipali Automation Tester");
+	}
+
+}
